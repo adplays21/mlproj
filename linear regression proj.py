141c07d70f510bd9c616ec663eab3281ceb277cc
adplays21 <111368664+adplays21@users.noreply.github.com>
2023-05-09 18:52:54 +0530

    linear regression proj.py

--- linear regression proj.py
+++ linear regression proj.py
@@ -1,23 +1,14 @@
-#!/usr/bin/env python
-# coding: utf-8
 
-# ___
-# 
-# <a href='http://www.pieriandata.com'> <img src='../Pierian_Data_Logo.png' /></a>
-# ___
-# # Linear Regression Project
-# 
-# Congratulations! You just got some contract work with an Ecommerce company based in New York City that sells clothing online but they also have in-store style and clothing advice sessions. Customers come in to the store, have sessions/meetings with a personal stylist, then they can go home and order either on a mobile app or website for the clothes they want.
-# 
+# # Linear Regression Model Project//
+
+# PROJECT DESCCRIPTION:
+# Congratulations! You just got some contract work with an Ecommerce company based in New York City that sells clothing online but they also have in-store style and clothing advice sessions. 
+#Customers come in to the store, have sessions/meetings with a personal stylist, then they can go home and order either on a mobile app or website for the clothes they want.  
 # The company is trying to decide whether to focus their efforts on their mobile app experience or their website. They've hired you on contract to help them figure it out! Let's get started!
-# 
-# Just follow the steps below to analyze the customer data (it's fake, don't worry I didn't give you real credit card numbers or emails).
+ # Just follow the steps below to analyze the customer data (it's fake, don't worry I didn't give you real credit card numbers or emails).
 
-# ## Imports
-# ** Import pandas, numpy, matplotlib,and seaborn. Then set %matplotlib inline 
-# (You'll import sklearn as you need it.)**
+#  Imports
 
-# In[3]:
 
 
 import pandas as pd
@@ -35,30 +26,19 @@ get_ipython().run_line_magic('matplotlib', 'inline')
 # * Time on App: Average time spent on App in minutes
 # * Time on Website: Average time spent on Website in minutes
 # * Length of Membership: How many years the customer has been a member. 
-# 
-# ** Read in the Ecommerce Customers csv file as a DataFrame called customers.**
 
-# In[5]:
 
 
 customers=pd.read_csv('Ecommerce Customers')
 
 
-# **Check the head of customers, and check out its info() and describe() methods.**
-
-# In[6]:
+# **Check the head of customers;
 
 
 customers.head()
 
 
-# In[7]:
-
-
 customers.describe()
-
-
-# In[8]:
 
 
 customers.info()
@@ -67,19 +47,15 @@ customers.info()
 # ## Exploratory Data Analysis
 # 
 # **Let's explore the data!**
-# 
-# For the rest of the exercise we'll only be using the numerical data of the csv file.
-# ___
-# **Use seaborn to create a jointplot to compare the Time on Website and Yearly Amount Spent columns. Does the correlation make sense?**
 
-# In[280]:
+# **Use seaborn to create a jointplot to compare the Time on Website and Yearly Amount Spent columns. Does the correlation make sense?**
 
 
 sns.set_paletee('Gnbu_d')
 sns.set_style('whitegrid')
 
 
-# In[11]:
+
 
 
 sns.jointplot(x='Time on Website',y='Yearly Amount Spent',data=customers)
@@ -87,7 +63,6 @@ sns.jointplot(x='Time on Website',y='Yearly Amount Spent',data=customers)
 
 # ** Do the same but with the Time on App column instead. **
 
-# In[12]:
 
 
 sns.jointplot(x='Time on Website',y='Yearly Amount Spent',data=customers)
@@ -95,15 +70,12 @@ sns.jointplot(x='Time on Website',y='Yearly Amount Spent',data=customers)
 
 # ** Use jointplot to create a 2D hex bin plot comparing Time on App and Length of Membership.**
 
-# In[13]:
 
 
 sns.jointplot(x='Time on App',y='Length of Membership',kind='hex',data=customers)
 
 
-# **Let's explore these types of relationships across the entire data set. Use [pairplot](https://stanford.edu/~mwaskom/software/seaborn/tutorial/axis_grids.html#plotting-pairwise-relationships-with-pairgrid-and-pairplot) to recreate the plot below.(Don't worry about the the colors)**
-
-# In[14]:
+# **Let's explore these types of relationships across the entire data set. 
 
 
 sns.pairplot(customers)
@@ -111,7 +83,7 @@ sns.pairplot(customers)
 
 # **Based off this plot what looks to be the most correlated feature with Yearly Amount Spent?**
 
-# In[285]:
+# In[x]:
 
 
 #length of Membership
@@ -214,10 +186,10 @@ plt.ylabel('PREDICTED Y')
 
 
 # ## Evaluating the Model
-# 
-# Let's evaluate our model performance by calculating the residual sum of squares and the explained variance score (R^2).
-# 
-# ** Calculate the Mean Absolute Error, Mean Squared Error, and the Root Mean Squared Error. Refer to the lecture or to Wikipedia for the formulas**
+
+# Let's evaluate our model performance by calculating the residual sum of squares .
+
+# ** Calculate the Mean Absolute Error, Mean Squared Error, and the Root Mean Squared Error. 
 
 # In[43]:
 
@@ -229,9 +201,9 @@ print('RMSE:',np.sqrt(metrics.mean_squared_error(y_test,predictions)))
 
 
 # ## Residuals
-# 
+
 # You should have gotten a very good model with a good fit. Let's quickly explore the residuals to make sure everything was okay with our data. 
-# 
+
 # **Plot a histogram of the residuals and make sure it looks normally distributed. Use either seaborn distplot, or just plt.hist().**
 
 # In[45]:
@@ -241,7 +213,9 @@ sns.displot((y_test-predictions),bins=50);
 
 
 # ## Conclusion
-# We still want to figure out the answer to the original question, do we focus our efforst on mobile app or website development? Or maybe that doesn't even really matter, and Membership Time is what is really important.  Let's see if we can interpret the coefficients at all to get an idea.
+# We still want to figure out the answer to the original question, do we focus our efforst on mobile app or website development?
+#Or maybe that doesn't even really matter, and Membership Time is what is really important. 
+#Let's see if we can interpret the coefficients at all to get an idea.
 # 
 # ** Recreate the dataframe below. **
 
@@ -252,15 +226,25 @@ coeffecients=pd.DataFrame(lm.coef_,X.columns)
 coeffecients.columns=['Coeffecient']
 coeffecients
 
+#OUTPUT FOR PREDICTIVE ANALYSIS :
+#Coeffecient
+#Avg. Session Length	25.981550
+#Time on App	38.590159
+#Time on Website	0.190405
+#Length of Membership	61.279097
+
 
 # ** How can you interpret these coefficients? **
 
-# 
+# - Holding all other features fixed, a 1 unit increase in **Avg. Session Length** is associated with an **increase of 25.98 total dollars spent**.
+#- Holding all other features fixed, a 1 unit increase in **Time on App** is associated with an **increase of 38.59 total dollars spent**.
+#- Holding all other features fixed, a 1 unit increase in **Time on Website** is associated with an **increase of 0.19 total dollars spent**.
+#- Holding all other features fixed, a 1 unit increase in **Length of Membership** is associated with an **increase of 61.27 total dollars spent**.
 
 # **Do you think the company should focus more on their mobile app or on their website?**
 
-# *Answer here*
+# 
 
 # ## Great Job!
-# 
-# Congrats on your contract work! The company loved the insights! Let's move on.
+# # #END
+#THANK YOU
